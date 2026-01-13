520f9711361eb6b9f6fca6526419323f2e726c48
Caio-C8 <caiomgf50@gmail.com>
2026-01-13 20:06:20 -0300

    feat: defines the login route as public

--- apps/api/src/modules/auth/api/auth.controller.ts
+++ apps/api/src/modules/auth/api/auth.controller.ts
@@ -1,13 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, Get } from '@nestjs/common';
 import { LoginDto } from './dtos/login.dto';
 import { AuthService } from '../core/services/auth.service';
 import { LoginResponse } from '../core/models/login-response.interface';
 import { ResponseMessage } from 'src/common/decorators/response-message.decorator';
+import { Public } from '../../../common/decorators/public.decorator';
 
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
+  @Public()
   @Post('login')
   @ResponseMessage('Login realizado com sucesso!')
   async login(@Body() loginDto: LoginDto): Promise<LoginResponse> {
