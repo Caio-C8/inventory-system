4bbe7a0853d35a18abd7f81908dea55d040b1d41
Caio-C8 <caiomgf50@gmail.com>
2026-01-13 19:15:51 -0300

    refactor: configures interceptor and filter in the app module file

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './modules/auth/auth.module';
+import { APP_FILTER, APP_INTERCEPTOR } from '@nestjs/core';
+import { CustomResponse } from './common/interceptors/response.interceptor';
+import { HttpExceptionFilter } from './common/filters/http-exception-filter';
 
 @Module({
   imports: [
@@ -10,6 +13,15 @@ import { AuthModule } from './modules/auth/auth.module';
     AuthModule,
   ],
   controllers: [],
-  providers: [],
+  providers: [
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: CustomResponse,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
+    },
+  ],
 })
 export class AppModule {}
--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,14 +1,8 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { CustomResponse } from './common/interceptors/response.interceptor';
-import { HttpExceptionFilter } from './common/filters/http-exception-filter';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-
-  app.useGlobalInterceptors(new CustomResponse());
-
-  app.useGlobalFilters(new HttpExceptionFilter());
 
   await app.listen(process.env.PORT ?? 3001);
 }
