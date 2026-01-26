51a6924b421413c79e06efc5282c73ee83471329
caio cesar <caio@scmpm.local>
2026-01-26 07:31:41 -0300

    chore: adds a return type

--- apps/api/src/modules/sales/api/sales.controller.ts
+++ apps/api/src/modules/sales/api/sales.controller.ts
@@ -1,13 +1,14 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
 import { SaleService } from '../core/services/sale.service';
 import { CreateSaleDto } from './dtos/create-sale.dto';
+import { Sale } from '../core/models/sale.model';
 
 @Controller('sales')
 export class SaleController {
   constructor(private readonly saleService: SaleService) {}
 
   @Post()
-  async create(@Body() createSaleDto: CreateSaleDto) {
+  async create(@Body() createSaleDto: CreateSaleDto): Promise<Sale> {
     return this.saleService.create(createSaleDto);
   }
 }
