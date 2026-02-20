e92a14259e0bff08e4052a1823dba4f44e40a7a4
caio cesar <caio@scmpm.local>
2026-02-20 08:43:48 -0300

    feat: creates some new hooks

--- apps/web/src/features/inventory/hooks/use-batches.ts
+++ apps/web/src/features/inventory/hooks/use-batches.ts
@@ -4,15 +4,18 @@ import {
   Batch,
   GetBatchesInput,
   PaginatedResult,
+  UpdateBatchInput,
 } from '@repo/types';
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { AxiosError } from 'axios';
+import { toast } from 'sonner';
 
 export const useGetBatchesByProduct = (
   productId: number,
   params: GetBatchesInput,
 ) => {
   return useQuery({
-    queryKey: ['batches', params],
+    queryKey: ['batches', productId, params],
     queryFn: async () => {
       const response = await api.get<ApiResponse<PaginatedResult<Batch>>>(
         `/batches/product/${productId}`,
@@ -24,5 +27,81 @@ export const useGetBatchesByProduct = (
       return response.data.data;
     },
     enabled: !!productId,
+  });
+};
+
+export const useUpdateBatch = (id: number, productId: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async (data: UpdateBatchInput) => {
+      const response = await api.patch(`/batches/${id}`, data);
+
+      return response.data;
+    },
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({ queryKey: ['batches'] });
+      queryClient.invalidateQueries({ queryKey: ['batch', id] });
+
+      queryClient.invalidateQueries({ queryKey: ['product', productId] });
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+
+      toast.success(response.data?.message || 'Lote atualizado com sucesso');
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(
+        error.response?.data.message || 'Ocorreu um erro inesperado.',
+      );
+    },
+  });
+};
+
+export const useSyncStockByProduct = (productId: Number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async () => {
+      const response = await api.patch(`/batches/${productId}/sync`);
+
+      return response.data;
+    },
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({ queryKey: ['product', productId] });
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+
+      toast.success(
+        response.data?.message || 'Estoque sincronizado com sucesso',
+      );
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(
+        error.response?.data.message || 'Ocorreu um erro inesperado.',
+      );
+    },
+  });
+};
+
+export const useDeleteBatch = (id: number, productId: number) => {
+  const queryClient = useQueryClient();
+
+  return useMutation({
+    mutationFn: async () => {
+      const response = await api.delete(`/batches/${id}`);
+
+      return response.data;
+    },
+    onSuccess: (response) => {
+      queryClient.invalidateQueries({ queryKey: ['batches'] });
+
+      queryClient.invalidateQueries({ queryKey: ['product', productId] });
+      queryClient.invalidateQueries({ queryKey: ['products'] });
+
+      toast.success(response.data?.message || 'Lote excluído com sucesso');
+    },
+    onError: (error: AxiosError<any>) => {
+      toast.error(
+        error.response?.data.message || 'Ocorreu um erro inesperado.',
+      );
+    },
   });
 };
