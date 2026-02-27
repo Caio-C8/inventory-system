63597a9013e311c4ff70098b9c54f3f862af536e
caio cesar <caio@scmpm.local>
2026-02-27 17:13:48 -0300

    feat: adds classes to style the disabled inuput

--- apps/web/src/components/ui/input.tsx
+++ apps/web/src/components/ui/input.tsx
@@ -10,8 +10,8 @@ function Input({ className, type, ...props }: React.ComponentProps<'input'>) {
       className={cn(
         'file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input h-9 w-full min-w-0 rounded-md border border-[#E4E4E7] bg-[#ffffff] px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:bg-[#F5F5F5] disabled:text-[#18181B] md:text-sm',
         'focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]',
-        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive',
-        'dark:border-[#2A2A2D] dark:bg-[#18181B] dark:disabled:text-[#FAFAFA]',
+        'aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive disabled:',
+        'dark:border-[#2A2A2D] dark:bg-[#18181B] dark:disabled:text-[#FAFAFA] dark:disabled:bg-[#121212]',
         className,
       )}
       {...props}
